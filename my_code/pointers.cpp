1a4feb9ea0069072d254a09239cfaeab84096054
RainaKim <hrk296@nyu.edu>
2019-06-07 11:34:06 -0400

    assignment3

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,33 +4,62 @@
 using namespace std;
 
 void inc_by_value(int n) {
+	n++;
 }
 
 
 void inc_with_pointer(int* iptr) {
+	(*iptr)++;
 }
 
 
 void inc_with_reference(int& n) {
+	n++;
 }
 
 
 Thing** create_array_of_things(int n) {
-    return nullptr;
+	Thing** thing = new Thing* [n];
+	for (int i=0; i<n; i++){
+		thing[i]=new Thing(i);
+	}
+    return thing;
 }
 
 void print_all_things(Thing** things, int n) {
+	for (int i=0; i<n; i++){
+		cout << things[i]->val << " ";
+	}
+	cout << endl;
 }
 
 void double_all_things(Thing** things, int n) {
+	for (int i=0; i<n; i++){
+		things[i]->val*=2;
+	}
 }
 
 void delete_all_things(Thing** things, int n) {
+	for (int i=0; i<n; i++){
+		delete things[i];
+		things[i] = nullptr;
+	}
+	delete[] things;
+	things = nullptr;
 }
 
 
-void assignTA(Student& s, Student& ta) {
+void assignTA(Student* s, Student* ta) {
+	s -> ta = ta;
 }
 
-void printTAs(vector<Student> students) {
+void printTAs(vector<Student*>& students) {
+	for (Student* student : students){
+		if (student -> ta == nullptr){
+			cout << student->name << " has no TA\n";
+		}
+		else {
+			cout << student->name << student->ta->name << endl;
+		}
+	}
 }
