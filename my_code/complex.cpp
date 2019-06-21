859d4eb212c4371a400b0414d251d4abe339985b
RainaKim <hrk296@nyu.edu>
2019-06-21 13:10:46 -0400

    cpp3
    cpp3

--- my_code/complex.cpp
+++ my_code/complex.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 bool operator== (const Complex& c1, const Complex& c2) {
-    if ((abs((c1.get_real() - c2.get_real())) < 0.0001) && (abs((c2.get_imag() - c2.get_real())) < 0.0001)){
+    if ((abs((c1.get_real() - c2.get_real())) < 0.0001) && (abs((c1.get_imag() - c2.get_imag())) < 0.0001)){
             return true;
         }
     return false;  // temp!
